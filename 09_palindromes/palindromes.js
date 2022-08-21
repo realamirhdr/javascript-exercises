f6d058731c3b8313114747e8bccf8b11fb272985
amir <lithiumhdr@gmail.com>
2022-08-21 23:50:25 +0430

    palidromes done!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    str = str.replace(/\s/g, '');
+    let temp = ""
+    for (let i = str.length; i >= 0; i--) {
+        temp += str.charAt(i);
+    }
+    if (str === temp) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
