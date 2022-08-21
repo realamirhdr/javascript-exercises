43302c9a3366647d75e088fb21cf0a158ecefbf6
amir <lithiumhdr@gmail.com>
2022-08-21 19:23:41 +0430

    leapYears done!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(yearNumber) {
+    if (yearNumber % 400 == 0) {
+        return true;
+    } else if (yearNumber % 100 != 0 && yearNumber % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
