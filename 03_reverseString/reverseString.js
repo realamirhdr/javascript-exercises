9f0252673cbe9878043729e99f9e52ddca361b5e
amir <lithiumhdr@gmail.com>
2022-08-21 17:56:46 +0430

    reverseString done!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringArray = []
+    for (let i = str.length - 1; i >= 0; i--) {
+        stringArray.push(str.charAt(i));
+    }
+    let result = "";
+    for (let i = 0; i < stringArray.length; i++) {
+        result += stringArray[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
