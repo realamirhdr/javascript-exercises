3cce68dcda335b131c3482f468d139951d9ceddd
amir <lithiumhdr@gmail.com>
2022-08-21 19:41:12 +0430

    tempConversion done!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = Math.round(((temp - 32) * 5 / 9) * 10);
+  return result / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result =  Math.round(((temp * 9 / 5) + 32) * 10);
+  return result / 10;
 };
+
+console.log(ftoc(100));
 
 // Do not edit below this line
 module.exports = {
