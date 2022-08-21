8d39d485c1c7c59c8257a61695d825684187ee64
amir <lithiumhdr@gmail.com>
2022-08-21 23:27:55 +0430

    calculator done!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let sum = 0;
+    for (let i in array) {
+        sum += array[i];
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let result = 1;
+    for (let i in array) {
+        result = result * array[i];
+    }
+    return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    let result = 1;
+    for (let i = 0; i < pow; i++) {
+        result = result * num;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
